b4f45054a6a7fee5af244d36b2be3259e22e92be
Tim Menzies <timm@ieee.org>
2023-12-06 07:49:17 -0500

    save

--- src/index.lua
+++ src/index.lua
@@ -1,5 +1,5 @@
--- <img src="refactor.png" width=400><br>
--- [home](index.html) :: [github](https://github.com/timm/refactor) ::
--- [issues](https://github.com/timm/refactor/issues) :: [lib](lib.html)
+-- <img src="min.png" width=400><br>
+-- [home](index.html) :: [github](https://github.com/timm/min) ::
+-- [issues](https://github.com/timm/min/issues) :: [lib](lib.html)
 -- :: start here &rightarrow; [zero](zero.html)
 VERSION=1.0
