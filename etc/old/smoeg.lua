890106f12b2e2faba06d5236a4183c5ccfc677bd
Tim Menzies <timm@users.noreply.github.com>
2023-12-24 14:15:15 -0500

    save

---
+++ etc/old/smoeg.lua
@@ -0,0 +1,59 @@
+-- vim: set et sts=2 sw=2 ts=2 
+local smo = require"smo"
+local l = require"smolib"
+local o,oo = l.o,l.oo
+local the,COLS,DATA,NUM,SYM = smo.the,smo.COLS,smo.DATA,smo.NUM,smo.SYM
+
+local eg={}
+
+function eg.oo()   oo{a=1,b=2,c=3,d={e=3,f=4}} end
+function eg.the() oo(the) end 
+function eg.help() print(the._help) end
+
+function eg.sym(      s,mode,e)
+  s = SYM()
+  for _, x in pairs{1,1,1,1,2,2,3} do s:add(x) end
+  mode, e = s:mid(), s:div()
+  print(mode, e)
+  return 1.37 < e and e < 1.38 and mode == 1 end
+
+local function norm(mu,sd,    R)
+  R=math.random
+  return (mu or 0) + (sd or 1) * math.sqrt(-2 * math.log(R()))
+                               * math.cos(2 * math.pi * R()) end
+
+function eg.num(      e,mu,sd)
+  e = NUM()
+  for _ = 1,1000 do e:add(norm(10, 2)) end
+  mu, sd = e:mid(), e:div()
+  print(l.rnd(mu,3), l.rnd(sd,3))
+  return 9.9 < mu and mu < 10 and 1.95 < sd and sd < 2 end
+
+function eg.csv()
+  for i,t in l.csv(the.file) do
+    if i%100 == 0 then print(i, o(t)) end end end
+
+function eg.data(     d)
+  d = DATA(the.file)
+  for i, t in pairs(d.rows) do
+    if i % 100 ==0 then oo(t) end end 
+  oo(d.cols.x[1]) end
+
+function eg.bayes(     kl,d,datas,acc)
+  for k=0,3 do
+    the.k = k
+    for m=0,3 do
+      the.m = m
+      d = DATA(the.file)
+      acc,datas = 0,{}
+      for _,row in pairs(d.rows) do
+        kl = row[d.cols.klass.at]
+        if kl == smo.likes(row, datas, #d.rows, 2) then acc=acc+1 end
+        datas[kl] = datas[kl] or DATA{d.cols.names}
+        datas[kl]:add(row) end
+      print(l.fmt("%5.2f\t%s\t%s",acc/#d.rows,k,m)) end end end
+
+l.cli(the)
+math.randomseed(the.seed)
+print(eg[the.todo]()==false and "❌ FAIL" or "✅ PASS")
+l.rogues()
---
+++ etc/old/smolib.lua
@@ -0,0 +1,86 @@
+-- vim: set et sts=2 sw=2 ts=2 
+local b4={}; for k, _ in pairs(_ENV) do b4[k]=k end
+local l={}
+
+-- -----------------------------------------------------------------------------
+-- ## Objects
+function l.obj(s,    t)
+  t = {a=s}
+  t.__index = t
+  return setmetatable(t, {
+    __call=function(_,...)
+      local self = setmetatable({},t)
+      return setmetatable(t.new(self,...) or self,t) end}) end
+
+-- -----------------------------------------------------------------------------
+-- ## Linting
+function l.rogues()
+  for k,v in pairs(_ENV) do if not b4[k] then print("E:",k,type(k)) end end end
+
+-- -----------------------------------------------------------------------------
+-- ## Nums
+function l.rnd(n, ndecs)
+  if type(n) ~= "number" then return n end
+  if math.floor(n) == n  then return n end
+  local mult = 10^(ndecs or 3)
+  return math.floor(n * mult + 0.5) / mult end
+
+-- -----------------------------------------------------------------------------
+-- ## Lists
+function l.keys(t,    u)
+  u={}; for k,v in pairs(t) do u[1+#u]=k end; table.sort(u); return u end
+
+function l.copy(t,    u)
+  if type(t) ~= "table" then return t end
+  u={}; for k,v in pairs(t) do u[l.copy(k)] = l.copy(v) end
+  return u end 
+
+-- -----------------------------------------------------------------------------
+-- ## String to Things
+function l.coerce(s1,    fun) 
+  function fun(s2)
+    if s2=="nil" then return nil else return s2=="true" or (s2~="false" and s2) end end
+  return math.tointeger(s1) or tonumber(s1) or fun(s1:match'^%s*(.*%S)') end
+
+function l.settings(s,    t,pat)
+  t,pat = {}, "\n[%s]+[-][%S][%s]+[-][-]([%S]+)[^\n]+= ([%S]+)"
+  for k, s1 in s:gmatch(pat) do t[k] = l.coerce(s1) end
+  t._help = s
+  return t end
+
+function l.words(s,   t)
+  t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=l.coerce(s1) end; return t end
+
+function l.csv(src,    i)
+  i,src = 0,src=="-" and io.stdin or io.input(src)
+  return function(      s)
+    s=io.read()
+    if s then i=i+1; return i,l.words(s) else io.close(src) end end end
+
+function l.cli(t)
+  for k, v in pairs(t) do
+    v = tostring(v)
+    for argv,s in pairs(arg) do
+      if s=="-"..(k:sub(1,1)) or s=="--"..k then
+        v = v=="true" and "false" or v=="false" and "true" or arg[argv + 1]
+        t[k] = l.coerce(v) end end end
+  if t.help then os.exit(print("\n"..t._help)) end
+  return t end
+
+-- -----------------------------------------------------------------------------
+-- Things to Strings
+l.fmt = string.format
+
+function l.oo(x) print(l.o(x)); return x end
+
+function l.o(t,  n,      u)
+  if type(t) == "number" then return tostring(l.rnd(t, n)) end
+  if type(t) ~= "table"  then return tostring(t) end
+  u={}
+  for _,k in pairs(l.keys(t)) do
+    if tostring(k):sub(1,1) ~= "_" then
+      u[1+#u]= #t>0 and l.o(t[k],n) or l.fmt("%s: %s", l.o(k,n), l.o(t[k],n)) end end
+  return "{" .. table.concat(u, ", ") .. "}" end
+
+-- -----------------------------------------------------------------------------
+return l
