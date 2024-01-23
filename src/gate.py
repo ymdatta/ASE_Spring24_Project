7834cf8e9a7309260914c8997e6057907d4b3ec6
Tim Menzies <timm@ieee.org>
2024-01-22 22:12:58 -0500

    sasas

--- src/gate.py
+++ src/gate.py
@@ -8,7 +8,7 @@ USAGE:
 
 OPTIONS:
   -c --cohen  small effect size               = .35
-  -f --file   csv data file name              = '../data/diabetes.csv'
+  -f --file   csv data file name              = '../data/auto93.csv'
   -h --help   show help                       = False
   -k --k      low class frequency kludge      = 1
   -m --m      low attribute frequency kludge  = 2
@@ -19,32 +19,19 @@ import re,sys,ast,math
 from collections import Counter
 from fileinput import FileInput as file_or_stdin
 
-def o(d,s=""): 
- return s+"{"+ (", ".join([f":{k} {v}" for k,v in d.items() if k[0]!="_"]))+"}" 
-
-class box(dict):
-  __getattr__ = dict.get
-  __setattr__ = dict.__setitem__
-  __repr__    = lambda x:o(x, x.__class__.__name__)
-
-def coerce(s):
-  try: return ast.literal_eval(s)
-  except Exception: return s
-
-the = box(**{m[1]:coerce(m[2]) for m in re.finditer(r"--(\w+)[^=]*=\s*(\S+)",__doc__)})
-
-def goalp(s):  return s[-1] in "+-!"
-def heaven(s): return 0 if s[-1] == "-" else 1
-def nump(s):   return s[0].isupper()
-
+#----------------------------------------------------------------------------------------
 def  DATA(lsts):
+  def goalp(s):  return s[-1] in "+-!"
+  def heaven(s): return 0 if s[-1] == "-" else 1
+  def nump(s):   return s[0].isupper()  
   def d2h(lst):
     return (sum(abs(w - norm(all[c],lst[c]))**2 for c,w in ys.items()) / len(ys))**.5
+  #-----------------
   names,*rows = lsts
   ys   = {c:heaven(s) for c,s in enumerate(names) if goalp(s)}
   nums = [c           for c,s in enumerate(names) if nump(s)]
   all  = [[y for y in x if y !="?"] for x in zip(*rows)]
-  all  = [NUM(a) if c in nums else Counter(a) for c,a in enumerate(all)]
+  all  = [(NUM(a) if c in nums else Counter(a)) for c,a in enumerate(all)]
   return box(rows = sorted(rows, key=d2h), 
              cols = box(names=names, ys=ys, nums=nums, all=all))
 
@@ -52,18 +39,45 @@ def clone(data,rows=[]):
   return DATA( [data.cols.names] +  rows )  
 
 def NUM(a):
-  a.sort()
-  p = len(a)/10
-  return box(n=len(a),lo=a[0], hi=a[-1], mid=a[int(p*5)], sd=(a[int(9*p)]-a[int(p)])/2.56)
+  n,sd,sum, lo,hi = 0,0,0, sys.maxsize, -sys.maxsize
+  for x in a: n  += 1; sum += x; hi=max(x,hi); lo=min(x,lo)
+  for x in a: sd += (x-sum/n)**2 
+  return box(n=n,lo=lo, hi=hi, mu=sum/n, sd= (sd/(n-1))**.5)
+
+def like(data,row,all,nh,m=1,k=2):
+  def num(col,x):
+    v = col.sd**2 + 10**-64
+    nom = math.e**(-1*(x - col.mu)**2/(2*v)) + 10**-64
+    denom = (2*math.pi*v)**.5
+    return min(1, nom/(denom + 10**-64))
+  def sym(col,x):
+    return (col.get(x, 0) + m*prior) / (sum(col.values()) + m)
+  #------------------------------------------
+  prior = (len(data.rows) + k) / (all + k*nh)
+  out   = math.log(prior)
+  for c,x in enumerate(row):
+    if x != "?" and c not in data.cols.ys:
+      col  = data.cols.all[c]
+      inc  = (sym if isinstance(col, Counter) else num)(col, x)
+      print(inc)
+      out += math.log(inc)
+  return out
+
+#----------------------------------------------------------------------------------------
+def o(d,s=""): 
+ return s+"{"+ (", ".join([f":{k} {v}" for k,v in d.items() if k[0]!="_"]))+"}" 
+
+class box(dict):
+  __getattr__ = dict.get
+  __setattr__ = dict.__setitem__
+  __repr__    = lambda x : o(x, x.__class__.__name__)
+
+def coerce(s):
+  try: return ast.literal_eval(s)
+  except Exception: return s
 
 def norm(col,x):
   return x if x=="?" else (x - col.lo)/(col.hi - col.lo + 1E-30) 
-
-def ent(d):
-  e,n = 0,0
-  for k in d: n += d[k]
-  for k in d: e += d[k]/n * math.log( d[k]/n, 2)
-  return -e
 
 def cli(d):
   for k,v in d.items(): 
@@ -82,7 +96,20 @@ def csv(file=None):
       line = re.sub(r'([\n\t\r"\’ ]|#.*)', '', line)
       if line: yield [coerce(s.strip()) for s in line.split(",")]
 
+#----------------------------------------------------------------------------------------
+class Eg:
+  def all():
+    sys.exit(sum((f() or 0) for f in [Eg.data, Eg.likes]))
+    
+  def data():
+    for i,row in enumerate(DATA([r for r in csv(the.file)]).rows):
+       if i % 30 == 0 : print(i,row)
 
-#---------------------------------------------------------------------------
+  def likes():
+    d = DATA([r for r in csv(the.file)])
+    for row in d.rows: like(d, row, 1000, 2, m=the.m, k=the.k)
+
+#----------------------------------------------------------------------------------------
+the = box(**{m[1]:coerce(m[2]) for m in re.finditer(r"--(\w+)[^=]*=\s*(\S+)",__doc__)})
 the = cli(the)
-DATA([r for r in csv(the.file)])
+getattr(Eg, the.todo)()
